03bbde7170ce80b17ede7987741685a1dbdff524
HairongWu <wuhairongqq@hotmail.com>
2024-06-29 10:14:28 +0800

    bug fix

--- studio/web/apps/labelstudio/src/pages/Settings/StorageSettings/StorageSet.jsx
+++ studio/web/apps/labelstudio/src/pages/Settings/StorageSettings/StorageSet.jsx
@@ -80,13 +80,6 @@ export const StorageSet = ({ title, target, rootClass, buttonLabel }) => {
             }}
           />
         ),
-        footer: (
-          <>
-            Save completed annotations to Amazon S3, Google Cloud, Microsoft Azure, or Redis.
-            <br />
-            <a href="https://labelstud.io/guide/storage.html">See more in the documentation</a>.
-          </>
-        ),
       });
     },
     [project, fetchStorages, target, rootClass],
